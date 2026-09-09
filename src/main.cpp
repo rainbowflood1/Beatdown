865e48b67b6ab046ceef2c19e535ee1e241505d1
Logical <88160870+rainbowflood1@users.noreply.github.com>
2026-09-09 18:24:03 -0400

    Added MainMenu function and calls it every 24 frames until stop is equal to true

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <TigerUI/TigerUI.hpp>
 #include <vector>
 #include <cstdint>
+#include <thread>
+#include <chrono>
+
+TigerUI tui;
+uint32_t fps = 24;
+bool stop = false;
+
+void MainMenu() {
+	
+}
 
 int main() {
+	while (stop == false) {
+		std::this_thread::sleep_for(std::chrono::seconds(1/fps));
+		MainMenu();
+	}
 	return 0;
 }
